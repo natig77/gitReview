774ca0d5350bdd84d96285afaf35a6ccfb6ff132
natig77 <natiggurbanov@gmail.com>
2022-05-31 21:35:28 -0400

    1st commit

---
+++ src/gitReview/getreview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+class getreview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("today we are learning Java");
+		System.out.println("Java is very interesting");
+
+	}
+
+}
