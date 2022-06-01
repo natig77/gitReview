3ea6881d07a218709e61c46f8544848fe643a2a5
natig77 <natiggurbanov@gmail.com>
2022-05-31 21:52:26 -0400

    2nd commit

--- src/gitReview/getreview01.java
+++ src/gitReview/getreview01.java
@@ -6,6 +6,8 @@ class getreview01 {
 		// TODO Auto-generated method stub
 		System.out.println("today we are learning Java");
 		System.out.println("Java is very interesting");
+		
+		System.out.println("I made some changes");
 
 	}
 
